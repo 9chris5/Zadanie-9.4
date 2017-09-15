9c9512acfad9b0cee0f8ede884cce0eedc431b04
Krzysiu <kheciek@gmail.com>
2017-09-15 16:39:26 +0200

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+var height = prompt ('Podaj wysokość choinki:');
+
+function drawTree(h) {
+    for ( var i = 0 ; i < h ; i++) {
+        var star = '*';
+        for ( var j = 0 ; j < i ; j++) {
+            star += '*';
+        }
+        console.log(star);
+    }
+}
+
+drawTree(height);
